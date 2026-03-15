56af2802ebab8a0d4c88bba53a6596ff4ee7a845
efbrace <ebenezerflintwoodbrace@gmail.com>
2026-03-15 12:19:22 +0000

    feat(web): Build Footer section

--- frontend/src/assets/svg/index.ts
+++ frontend/src/assets/svg/index.ts
@@ -1,5 +1,4 @@
 import { AltairLogo } from "./AltairLogo";
-import footerBgPattern from "./footer-bg-pattern.svg";
 import WTWCLogoWhite from "./WTWC-logo-white.svg";
 
-export { AltairLogo, footerBgPattern, WTWCLogoWhite };
+export { AltairLogo, WTWCLogoWhite };
